09cb21feae4cc9b7a91ad3f20b0cb0fd9d224182
adnannavaz3 <92943178+adnannavaz3@users.noreply.github.com>
2022-10-30 20:00:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ const product = require('./Routes/products-route');
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-.then(() => console.log('connected to mongoDB'))
-.catch(err => console.log(err));
+    })
+    .then(() => console.log('connected to mongoDB'))
+    .catch(err => console.log(err));
 
 const app = express();
 
@@ -20,10 +20,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(morgan("dev"));
-
 app.use('/api/products', product);
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
     console.log(`listening on PORT:${PORT}`)
 });
